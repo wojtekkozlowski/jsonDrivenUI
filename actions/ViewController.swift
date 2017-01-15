f335e148c88dcbb2bf90a4a38b2317cf955efd79
Wojtek Kozlowski <maniacus.gd@gmail.com>
2017-01-15 15:06:21 +1100

    added textview

--- actions/ViewController.swift
+++ actions/ViewController.swift
@@ -10,13 +10,38 @@ import UIKit
 import ObjectMapper
 
 class ViewController: UIViewController {
+    
+    let ut = UITextView()
 
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
-        let m = Mapper<ActionModel>().map(JSONString: readJson())!
-        var action = ActionModelToAction(model: m).convert()
-        action.pvc = self
-        ActionHandler(action: action).start()
+        
+        self.view.addSubview(ut)
+        ut.layer.borderColor = UIColor.black.cgColor
+        ut.layer.borderWidth = 2
+        ut.text = readJson()
+        ut.translatesAutoresizingMaskIntoConstraints = false
+    
+        let b = UIButton()
+        b.translatesAutoresizingMaskIntoConstraints = false
+        self.view.addSubview(b)
+        self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "V:|-(30)-[textView]-[button(40)]-(20)-|", options: [], metrics: nil, views: ["textView":ut, "button":b]))
+        self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[textView]-|", options: [], metrics: nil, views: ["textView":ut]))
+        self.view.addConstraints(NSLayoutConstraint.constraints(withVisualFormat: "H:|-[button]-|", options: [], metrics: nil, views: ["button":b]))
+        b.setTitle("execute", for: .normal)
+        b.backgroundColor = .blue
+        b.addTarget(self, action: #selector(bp), for: .touchUpInside)
+    }
+    
+    func bp(){
+        if let m = Mapper<ActionModel>().map(JSONString: ut.text!) {
+            var action = ActionModelToAction(model: m).convert()
+            action.pvc = self
+            ActionHandler(action: action).start()
+        } else {
+            print("wrong json")
+        }
+        
     }
 }
 
